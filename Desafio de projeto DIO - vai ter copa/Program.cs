75099ee956c8856e580c0a6de70ff5cf17c804de
weverton fagner <wevertoonfagneeer@outlook.com.br>
2022-01-12 22:06:03 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio de projeto DIO - vai ter copa/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Desafio_de_projeto_DIO___vai_ter_copa
+{
+        class DIO
+        {
+
+            static void Main(string[] args)
+            {
+
+                string str;
+                while ((str = Console.ReadLine()) != null)
+                {
+                    int x = int.Parse(str);
+                    if (x == 0)
+                    {
+                        Console.WriteLine("vai ter copa!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("vai ter duas!");
+                    }
+
+                }
+
+            }
+
+        }
+}
